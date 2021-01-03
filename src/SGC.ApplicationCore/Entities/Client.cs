5051c82b24ac8bdbec4661dc3bf553ba41c39871
Leandro Castro <leandrocastrold@hotmail.com>
2021-01-03 18:29:46 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGC.ApplicationCore/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entities
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public int ClientID { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+
+    }
+}
